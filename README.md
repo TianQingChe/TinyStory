0de9c0fff2bad57131a7cccdefba4951b6f4d81b
TianQingChe <tianqingcheyyf@gmail.com>
2018-09-19 15:52:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TinyURL
+
+The implementation of webserver based on node.js.
+
+This project provide url shortening service for user.
+
+The project follows the requirement of RESTful.
---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest');
+var redirectRouter = require('./routes/redirect');
+
+app.use("/api/v1", restRouter);
+
+app.use("/:shortUrl", redirectRouter);
+app.get("/", function (req, res) {
+    res.send("hello express world");
+});
+
+
+app.listen(3000);
+
+
+
+
+
+
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function (req, res) {
+   if (req.url === "/") {
+       res.writeHead(200, {"Content-Type": "text-html"});
+       var html = fs.readFileSync(__dirname + "/index.html");
+       res.end(html);
+   }
+
+   if (req.url === "/api") {
+       res.writeHead(200, {"Content-Type": "application/json"});
+       var obj = {
+           name: "Zemin Jiang",
+           age: 12
+       }
+       res.end(JSON.stringify(obj));
+   }
+}).listen(3000);
